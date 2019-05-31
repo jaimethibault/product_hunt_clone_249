57d10610c39994d4372d22c47a8b11b3f8cea444
Thibault Jaime <jaime.thibault@gmail.com>
2019-05-31 09:34:02 +0200

    test travis CI

---
+++ test/system/products_test.rb
@@ -0,0 +1,10 @@
+require "application_system_test_case"
+
+class ProductsTest < ApplicationSystemTestCase
+  test "visiting the index" do
+    visit "/"
+    save_and_open_screenshot
+    assert_selector "h1", text: "Awesome Products"
+    assert_selector ".product", count: Product.count
+  end
+end
